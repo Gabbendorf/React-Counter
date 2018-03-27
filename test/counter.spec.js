9fa0b81bf07ae999c292c25eef738b7517cea03e
Gabba <gmedas@8thlight.com>
2018-03-27 16:22:13 +0100

    DRYs up setting up for each test.

--- test/counter.spec.js
+++ test/counter.spec.js
@@ -3,26 +3,29 @@ import ReactDOM from 'react-dom';
 import { shallow, mount } from 'enzyme';
 import { Counter } from '../src/counter'
 
+let counter
+
+beforeEach(() => {
+  counter = shallow(<Counter />)
+})
+
 test('renders without crashing', () => {
   mount(<Counter />);
 })
 
 test('renders a button component', () => {
-  const counter = shallow(<Counter />);
   const button = counter.find("button");
 
   expect(button.text()).toBe("+1");
 });
 
 test('initial count value is zero', () => {
-  const counter = shallow(<Counter />);
   const h1 = counter.find("h1");
 
   expect(h1.text()).toBe("0");
 });
 
 test('sets updated count', () => {
-  const counter = shallow(<Counter />);
   counter.find("button").simulate("click");
 
   const h1 = counter.find("h1");
